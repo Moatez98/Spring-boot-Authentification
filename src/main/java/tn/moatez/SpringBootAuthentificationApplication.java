ad47fdeee07ec9c1bf221c36bb4da7e910fb1a14
moatez98 <reh.moatez@gmail.com>
2024-02-12 13:14:37 +0100

    first commit

---
+++ src/main/java/tn/moatez/SpringBootAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package tn.moatez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthentificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/moatez/SpringBootAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.moatez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
